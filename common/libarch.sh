388d1c09eae6c117c40f2945a17b026fd090553c
Justin Yang <linuxjustin@gmail.com>
2018-08-31 16:21:31 +0800

    It should be run in root

--- common/libarch.sh
+++ common/libarch.sh
@@ -20,7 +20,7 @@ function recover_packages {
 
 function add_pacman_mirror {
     echo "Add fast mirror for pacman..."
-    sed -i '1s/^/Server = https\:\/\/mirrors\.tuna\.tsinghua\.edu\.cn\/archlinuxarm\/\$arch\/\$repo \n/' /etc/pacman.d/mirrorlist
+    sudo sed -i '1s/^/Server = https\:\/\/mirrors\.tuna\.tsinghua\.edu\.cn\/archlinuxarm\/\$arch\/\$repo \n/' /etc/pacman.d/mirrorlist
     head /etc/pacman.d/mirrorlist
 }
 
