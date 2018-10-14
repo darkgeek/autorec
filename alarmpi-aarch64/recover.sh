9d1889484de80736de4cb3d32d021c0a72e6f4db
Justin Yang <linuxjustin@gmail.com>
2018-10-14 13:05:56 +0800

    We now boot pi from USB, so need to fix fstab

--- alarmpi-aarch64/recover.sh
+++ alarmpi-aarch64/recover.sh
@@ -26,7 +26,7 @@ sudo cp resolved.conf /etc/systemd/
 sudo cp -r binfmt.d /usr/lib/
 echo "load-module module-simple-protocol-tcp source=0 record=true port=12345" | sudo tee -a /etc/pulse/default.pa
 echo "load-module module-native-protocol-unix socket=/tmp/pulse.socket" | sudo tee -a /etc/pulse/default.pa
-echo "/dev/mmcblk0p2  /   f2fs    defaults,noatime        0       0" | sudo tee -a /etc/fstab
+echo "/dev/sda2  /   ext4    defaults,noatime        0       0" | sudo tee -a /etc/fstab
 
 echo "Configure user group..."
 sudo usermod -a -G wheel,audio,video,input justin
