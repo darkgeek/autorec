88096632c0b95a298b0d054088ebe881db115879
Justin Yang <linuxjustin@gmail.com>
2018-11-24 02:54:17 +0800

    Fix frequent crash issue by redirecting output

--- openbsd-x86_64/monit.d/scripts/shadowsocks-go.sh
+++ openbsd-x86_64/monit.d/scripts/shadowsocks-go.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/justin/go/bin/shadowsocks-local -c /home/justin/Shares/private/shadowsocks/config.json &
+/home/justin/go/bin/shadowsocks-local -c /home/justin/Shares/private/shadowsocks/config.json > /dev/null 2>&1 &
