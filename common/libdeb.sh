28024b8158270cd8b3e4008da92a247be4719c1c
Justin Yang <linuxjustin@gmail.com>
2019-05-18 01:44:51 +0800

    Add support for mipse64el debian.

--- common/libdeb.sh
+++ common/libdeb.sh
@@ -11,3 +11,14 @@ function recover_package_list {
     echo "Recover packages..."
     xargs -0 apt install -y < <(tr \\n \\0 < $PKG_LIST)
 }
+
+function recover_internet {
+    grab_bridge_config
+    sudo mkdir /etc/shadowsocks
+    copy_bridge_config /etc/shadowsocks
+
+    grab_great_list
+    move_action_file /etc/privoxy/
+    add_actions_file /etc/privoxy/config
+}
+
---
+++ debian-mips64el/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_DIR=`dirname "$0"`
+SCRIPT_NAME=`basename "$0"`
+
+. $CURRENT_DIR/../common/libdeb.sh
+. $CURRENT_DIR/../common/libcomm.sh
+
+echo "Working dir is $CURRENT_DIR"
+
+cd $CURRENT_DIR
+backup_package_list
