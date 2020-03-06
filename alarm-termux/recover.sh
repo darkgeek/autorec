460e0564cf118b3fc4e1ecd84fd728b92caa9795
Justin Yang <linuxjustin@gmail.com>
2020-03-07 03:08:22 +0800

    Update for termux

--- alarm-termux/recover.sh
+++ alarm-termux/recover.sh
@@ -6,16 +6,16 @@ SCRIPT_NAME=`basename "$0"`
 . $CURRENT_DIR/../common/libarch.sh
 . $CURRENT_DIR/../common/libcomm.sh
 
-echo "Recover faked bins..."
-cp -r bin $HOME/
-echo "export PATH=$HOME/bin:\$PATH" >> $HOME/.bash_profile
-. $HOME/.bash_profile
-echo "Current PATH is $PATH"
+#echo "Recover faked bins..."
+#cp -r bin $HOME/
+#echo "export PATH=$HOME/bin:\$PATH" >> $HOME/.bash_profile
+#. $HOME/.bash_profile
+#echo "Current PATH is $PATH"
 
-echo "Patch makepkg..."
-cd $HOME/bin
-cp /usr/bin/makepkg .
-patch < $CURRENT_DIR/makepkg.patch
+#echo "Patch makepkg..."
+#cd $HOME/bin
+#cp /usr/bin/makepkg .
+#patch < $CURRENT_DIR/makepkg.patch
 
 echo "Recover packages..."
 echo "Working dir is $CURRENT_DIR"
