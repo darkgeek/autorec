51737b6b741d1e77ba7f1f5b597598316b956957
Justin Yang <linuxjustin@gmail.com>
2021-11-27 02:49:03 +0800

    Improvement on auto-paste to work better

--- debian-arm64/auto-paste.sh
+++ debian-arm64/auto-paste.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/fish -l
 
 echo "Input anything, Paste anywhere:"
 read user_input
 echo -n "$user_input" | xclip -selection clipboard
-xdotool windowminimize --sync $(xdotool getactivewindow)
-sleep 1
+xdotool windowminimize --sync (xdotool getactivewindow)
+sleep 0.5
 xdotool key ctrl+v
+#xvkbd -no-repeat -text "\Cv"
