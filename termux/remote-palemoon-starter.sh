6293fbe9c7687516a0e5d85dac92cd4232c496fa
Justin Yang <linuxjustin@gmail.com>
2022-05-02 18:42:52 +0800

    Now it can handle multiple pids returned by pgrep

--- termux/remote-palemoon-starter.sh
+++ termux/remote-palemoon-starter.sh
@@ -8,9 +8,11 @@ cp .Xauthority ../usr/var/lib/proot-distro/installed-rootfs/archlinux/home/justi
 echo Start palemoon...
 curl http://127.0.0.1:9900/palemoon/DISPLAY=localhost:10.0/PULSE_SERVER=127.0.0.1 && printf "\nDone.\n"
 
-running_pid=`pgrep -x palemoon`
-echo Palemoon is running as PID $running_pid.
+running_pids=`pgrep -x palemoon`
+echo Palemoon is running as PID $running_pids.
 
-# wait until running_pid is exited
-tail --pid=$running_pid -f /dev/null
+# wait until running_pids are all exited
+for pid in "$running_pids"; do
+    tail --pid=$pid -f /dev/null
+done
 echo Palemoon has exited. Bye.
