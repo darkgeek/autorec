e74b2a6def8e5a582ffb67fbde2571f29330ba3b
Justin Yang <linuxjustin@gmail.com>
2022-05-02 17:22:22 +0800

    Improve by using tail to wait util palemoon exits

--- termux/remote-palemoon-starter.sh
+++ termux/remote-palemoon-starter.sh
@@ -6,14 +6,11 @@ echo copy .Xauthority to proot distro inside
 cp .Xauthority ../usr/var/lib/proot-distro/installed-rootfs/archlinux/home/justin && echo Done.
 
 echo Start palemoon...
-curl http://127.0.0.1:9900/palemoon/DISPLAY=localhost:10.0/PULSE_SERVER=127.0.0.1
+curl http://127.0.0.1:9900/palemoon/DISPLAY=localhost:10.0/PULSE_SERVER=127.0.0.1 && printf "\nDone.\n"
 
-while [ true ]
-do
-    pgrep -x palemoon > /dev/null
-    if [ $? -ne 0 ]; then
-        echo \npalemoon has exited. Bye.
-        exit 0
-    fi
-    sleep 2
-done
+running_pid=`pgrep -x palemoon`
+echo Palemoon is running as PID $running_pid.
+
+# wait until running_pid is exited
+tail --pid=$running_pid -f /dev/null
+echo Palemoon has exited. Bye.
